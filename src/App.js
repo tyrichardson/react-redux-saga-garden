4150ab13dbe1c0fc5c28c15a3d004d011f13c3a5
Ty Richadson <tyLrichardson63@gmail.com>
2020-06-20 15:25:17 -0500

    initial commit to new repo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Garden from './components/Garden/Garden';
+
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1>Welcome to your garden!</h1>
+    </header>
+    <Garden />
+  </div>
+);
+
+export default App;
